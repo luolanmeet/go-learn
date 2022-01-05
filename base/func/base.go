6d934633bc6eae1db55c446ee4e08b64f0555806
chenken <mainem@163.com>
2022-01-06 00:43:35 +0800

    ignore

--- base/func/base.go
+++ base/func/base.go
@@ -1,28 +1,28 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 )
 
 func div0(a, b int) int {
-    return a / b
+	return a / b
 }
 
 /**
  * 可以返回多个值
- */ 
+ */
 func div1(a, b int) (int, int) {
-    return a / b, a % b
+	return a / b, a % b
 }
 
 /**
  * 可以指定返回值的名字
  */
 func div2(a, b int) (q, r int) {
-    // 下边的计算注释掉也可以正常运行，返回的是qr的零值
-    q = a / b
-    r = a % b
-    return
+	// 下边的计算注释掉也可以正常运行，返回的是qr的零值
+	q = a / b
+	r = a % b
+	return
 }
 
 /**
@@ -30,32 +30,31 @@ func div2(a, b int) (q, r int) {
  * 函数式编程
  */
 func apply(op func(int, int) int, a, b int) int {
-    return op(a, b)
+	return op(a, b)
 }
 
 /**
  * 可变参数列表
  */
-func sum(numbers ... int) int {
+func sum(numbers ...int) int {
 
-    sum := 0
-    for i := range numbers {
-        sum += numbers[i]
-    }
-    return sum;
+	sum := 0
+	for i := range numbers {
+		sum += numbers[i]
+	}
+	return sum
 }
-
 
 func main() {
 
-    fmt.Println(div1(5, 2))
-    fmt.Println(div2(5, 2))
-    
-    fmt.Println(apply(div0, 5, 2))
-    fmt.Println(apply(func(a, b int) int {
-        return a * b
-    }, 5, 2))
+	fmt.Println(div1(5, 2))
+	fmt.Println(div2(5, 2))
 
-    fmt.Println(sum(1, 2, 3))
+	fmt.Println(apply(div0, 5, 2))
+	fmt.Println(apply(func(a, b int) int {
+		return a * b
+	}, 5, 2))
+
+	fmt.Println(sum(1, 2, 3))
 
 }
---
+++ crawler/README.md
@@ -0,0 +1,5 @@
+
+go get golang.org/x/text
+
+go get golang.org/x/net/html
+
