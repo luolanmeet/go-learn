e6f5c551831dbd7944f6174c1fc54e04eaab8c69
chenken <mainem@163.com>
2022-01-08 19:15:15 +0800

    ignore

--- src/crawler/zhenai/parser/city.go
+++ src/crawler/zhenai/parser/city.go
@@ -5,7 +5,10 @@ import (
 	"regexp"
 )
 
-const cityRe = `<a href="(http://album.zhenai.com/u/[0-9]+)"[^>]*>([^<]+)</a>`
+// 只有姓名
+//const cityRe = `<a href="(http://album.zhenai.com/u/[0-9]+)"[^>]*>([^<]+)</a>`
+// 有姓名和性别
+const cityRe = `<a href="(http://album.zhenai.com/u/[0-9]+)"[^>]*>([^<]+)</a>[^性别：]*性别：</span>([^</td>]*)</td>`
 
 func ParseCity(contents []byte) engine.ParseResult {
 
@@ -17,9 +20,9 @@ func ParseCity(contents []byte) engine.ParseResult {
 		result.Items = append(result.Items, "User "+string(m[2]))
 		request := engine.Request{
 			Url: string(m[1]),
-			// 通过函数式变成，传递了name
+			// 通过函数式编程， 适配不兼容的接口
 			ParserFunc: func(c []byte) engine.ParseResult {
-				return ParseProfile(c, string(m[2]))
+				return ParseProfile(c, string(m[2]), string(m[3]))
 			},
 		}
 		result.Requests = append(result.Requests, request)
--- src/crawler/zhenai/parser/profile.go
+++ src/crawler/zhenai/parser/profile.go
@@ -7,10 +7,10 @@ import (
 
 //var ageRe = regexp.MustCompile("")
 
-func ParseProfile(contents []byte, name string) engine.ParseResult {
+func ParseProfile(contents []byte, name string, gender string) engine.ParseResult {
 	profile := model.Profile{}
 	profile.Name = name
-	profile.Gender = "?"
+	profile.Gender = gender
 	profile.Age = 1
 	profile.Height = 22
 	profile.Weight = 33
