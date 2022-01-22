d8d699744196c2ace06573eb9e3c5c42fd2c4092
chenken <mainem@163.com>
2022-01-22 11:27:15 +0800

    补充demo

--- base/container/array.go
+++ base/container/array.go
@@ -1,67 +1,87 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 )
 
 func define() {
 
-    var arr1 [5]int
-    // 用 := 必须给定值
-    arr2 := [3]int{1, 2, 3}
-    // 用...可以省略数组长度
-    arr3 := [...]int{1, 2}
-    // 3行4列
-    var arr4 [3][4]bool
+	var arr1 [5]int
+	// 用 := 必须给定值
+	arr2 := [3]int{1, 2, 3}
+	// 用...可以省略数组长度
+	arr3 := [...]int{1, 2}
+	// 3行4列
+	var arr4 [3][4]bool
 
-    fmt.Println(arr1, arr2, arr3)
-    fmt.Println(arr4)
+	fmt.Println(arr1, arr2, arr3)
+	fmt.Println(arr4)
 }
 
 func traversal() {
 
-    arr := [...]int{1, 2}
+	arr := [...]int{1, 2}
 
-    // 普通遍历
-    for i := 0; i < len(arr); i++ {
-        fmt.Println(arr[i])
-    }
-    // 利用range获取下标遍历
-    for i := range arr {
-        fmt.Println(arr[i])
-    }
-    // 遍历下标和值
-    for i, v := range arr {
-        fmt.Println(i, v)
-    }
-    // 遍历数组的值
-    for _, v := range arr {
-        fmt.Println(v)
-    }
+	// 普通遍历
+	for i := 0; i < len(arr); i++ {
+		fmt.Println(arr[i])
+	}
+	// 利用range获取下标遍历
+	for i := range arr {
+		fmt.Println(arr[i])
+	}
+	// 遍历下标和值
+	for i, v := range arr {
+		fmt.Println(i, v)
+	}
+	// 遍历数组的值
+	for _, v := range arr {
+		fmt.Println(v)
+	}
 
 }
 
 func printArray(arr *[5]int) {
 
-    // 这里不用写成 (*arr)[0]
-    fmt.Println(arr[0])
+	// 这里不用写成 (*arr)[0]
+	fmt.Println(arr[0])
 
-    for i, v := range arr {
-        fmt.Println(i, v)
-    }
+	for i, v := range arr {
+		fmt.Println(i, v)
+	}
+}
+
+func other() {
+	arr := [...]int{1: 1, 3: 4}
+	fmt.Printf("%v \n", arr)
+
+	// append 入参只能是切面，没法用数组，数组定义好之后长度就固定了
+	// 切面中指向的数组也不是原数组
+	s := append(arr[:], 5)
+	fmt.Printf("%v \n", s)
+	arr[0] = 100
+	fmt.Printf("%v \n", s)
+	fmt.Printf("%v \n", arr)
+
 }
 
 func main() {
 
-    define()
-    traversal()
+	define()
+	fmt.Println("===========")
 
-    // 数组也是值类型，调用函数是传入数组会发生数组拷贝
-    // [5]int 和 [3]int 是不一样的类型！
-    // go 语言中一般不直接使用数组，而是切片
-    arr := [5]int{1, 2, 3, 4, 5}
-    printArray(&arr)
-    // arr1 := [3]int{1, 2, 3}
-    // printArray(&arr1)
+	traversal()
+	fmt.Println("===========")
+	// 数组也是值类型，调用函数是传入数组会发生数组拷贝
+	// [5]int 和 [3]int 是不一样的类型！
+	// go 语言中一般不直接使用数组，而是切片
+	arr := [5]int{1, 2, 3, 4, 5}
+	printArray(&arr)
+	// arr1 := [3]int{1, 2, 3}
+	// printArray(&arr1)
+	fmt.Println("===========")
+
+	other()
+	fmt.Println("===========")
 
 }
--- base/container/slice.go
+++ base/container/slice.go
@@ -51,6 +51,7 @@ func extend() {
 }
 
 func testAppend() {
+
 	arr := [...]int{0, 1, 2, 3, 4, 5, 6, 7}
 	s1 := arr[2:6]
 	s2 := s1[3:5]
@@ -64,11 +65,56 @@ func testAppend() {
 	fmt.Println("arr = ", arr)
 }
 
+func other() {
+	// s1 切面内部指向的是不是原数组
+	arr := [...]int{0, 1, 2, 3, 4, 5, 6, 7}
+	s1 := arr[0:2]
+	fmt.Printf("%v \n", s1)
+	arr[0] = 100
+	s1[1] = 200
+	fmt.Printf("%v \n", s1)
+
+	// make 创建的切片
+	s2 := make([]int, 2)
+	fmt.Printf("%v \n", s2)
+	s2[0] = 1
+	fmt.Printf("%v \n", s2)
+}
+
+func testAppend2() {
+	s1 := make([]int, 3)
+	s1[0] = 0
+	s1[1] = 1
+	s1[2] = 2
+
+	// append 之后生成的slice，指向的是同个数组吗
+	// 不是
+	s2 := append(s1, 3)
+	fmt.Printf("%v \n", s2)
+	s1[0] = 100
+	fmt.Printf("%v \n", s2)
+
+	// 由切面产生的切面，指向的是同个数组吗
+	// 是
+	s3 := s1[0:1]
+	fmt.Printf("%v \n", s3)
+	s1[0] = 1000
+	fmt.Printf("%v \n", s3)
+}
+
 func main() {
 	base()
 	fmt.Println("=============")
+
 	extend()
 	fmt.Println("=============")
+
 	testAppend()
+	fmt.Println("=============")
+	other()
+	fmt.Println("=============")
+
 	// 切面不能作为返回值？
+	testAppend2()
+
 }
