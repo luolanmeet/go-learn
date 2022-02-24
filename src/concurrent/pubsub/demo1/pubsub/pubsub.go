a3d3d620798209569fd1c25e41eb8750b10575c8
chenken <mainem@163.com>
2022-02-24 23:56:28 +0800

    add demo

---
+++ src/concurrent/pubsub/demo1/pubsub/pubsub.go
@@ -0,0 +1,89 @@
+package pubsub
+
+import (
+	"sync"
+	"time"
+)
+
+// 定义了 订阅者和主题的 类型
+type (
+	subscriber chan interface{}         // 订阅者为一个管道
+	topicFunc  func(v interface{}) bool // 主题为一个过滤器
+)
+
+// Publisher 定义发布者的类型，为一个结构体
+type Publisher struct {
+	m           sync.RWMutex             // 读写锁
+	buffer      int                      // 订阅队列的缓存大小
+	timeout     time.Duration            // 发布超时时间
+	subscribers map[subscriber]topicFunc // 订阅者消息
+}
+
+// NewPublisher 构建一个发布者
+func NewPublisher(publishTimeOut time.Duration, buffer int) *Publisher {
+	return &Publisher{
+		buffer:      buffer,
+		timeout:     publishTimeOut,
+		subscribers: make(map[subscriber]topicFunc),
+	}
+}
+
+// Subscribe 添加一个新的订阅者，订阅全部主题
+func (p *Publisher) Subscribe() chan interface{} {
+	return p.SubscribeTopic(nil)
+}
+
+// SubscribeTopic 添加一个新的订阅者，订阅筛选后的主题
+func (p *Publisher) SubscribeTopic(topic topicFunc) chan interface{} {
+	ch := make(chan interface{}, p.buffer)
+	p.m.Lock()
+	p.subscribers[ch] = topic
+	p.m.Unlock()
+	return ch
+}
+
+// Exit 退出订阅
+func (p *Publisher) Exit(sub chan interface{}) {
+	p.m.Lock()
+	defer p.m.Unlock()
+	delete(p.subscribers, sub)
+	close(sub)
+}
+
+// Close 关闭发布者对象，同时关闭所有订阅者管道
+func (p *Publisher) Close() {
+	p.m.Lock()
+	defer p.m.Unlock()
+	for sub := range p.subscribers {
+		delete(p.subscribers, sub)
+		close(sub)
+	}
+}
+
+// 发送主题，容忍一定时间超时
+func (p *Publisher) sendTopic(
+	sub subscriber, topic topicFunc,
+	v interface{}, wg *sync.WaitGroup) {
+
+	defer wg.Done()
+	if topic != nil && !topic(v) {
+		return
+	}
+
+	select {
+	case sub <- v:
+	case <-time.After(p.timeout):
+	}
+}
+
+// 发布一个主题
+func (p *Publisher) Publish(v interface{}) {
+	p.m.Lock()
+	defer p.m.Unlock()
+	var wg sync.WaitGroup
+	for sub, topic := range p.subscribers {
+		wg.Add(1)
+		go p.sendTopic(sub, topic, v, &wg)
+	}
+	wg.Wait()
+}
